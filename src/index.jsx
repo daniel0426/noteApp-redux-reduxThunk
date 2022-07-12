162ddb011c6e52647cf2c7ba90eb9ed66f4f8e29
Daniel <angus9209@gmail.com>
2022-07-13 07:51:55 +1200

    make custom log middle ware for record action

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App';
 import {Provider} from 'react-redux';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import noteReducer from './store/modules/index';
+import loggerMiddleware from './lib/logger';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(noteReducer);
+const store = createStore(noteReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
